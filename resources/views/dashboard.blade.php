4346ee5bade218ebe26017752701422ef9ba9621
vagrant <vagrant@homestead>
2017-10-11 20:12:12 +0000

    Second Commit, login in, creating post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,48 @@
-<h1>The Dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+    <section class=""row new="post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="{{ route('post.create') }}" method="post">
+                <div class="form-group">
+                    <textarea class="form-control" name="body" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+                <input type="hidden" value="{{Session::token()}}" name="_token">
+            </form>
+
+        </div>
+
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>sadasfasgasgasgasgasgasgasgas</p>
+                <div vlass="info">
+                    Posted by me
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a>
+                    <a href="#">Dislike</a>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>sadasfasgasgasgasgasgasgasgas</p>
+                <div vlass="info">
+                    Posted by me
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a>
+                    <a href="#">Dislike</a>
+                    <a href="#">Edit</a>
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+
+@endsection
